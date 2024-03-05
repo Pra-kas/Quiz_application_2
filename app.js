478b6ce165b1c9f6436348017b91fb90121db024
kamaleshbala01 <kamalesravi54@gmail.com>
2024-03-05 10:18:22 +0530

    d

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const bodyParser = require('body-parser');
+const cors = require("cors");
+const userRouter = require('./router/userRouter');
+const loginRouter = require('./router/loginRouter');
+const quizRouter = require('./router/quizRouter');
+const getQuizRouter = require('./router/getQuizRouter');
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use('/',userRouter);
+app.use('/',loginRouter);
+app.use('/',quizRouter);
+app.use('/',getQuizRouter);
+
+module.exports = app;
